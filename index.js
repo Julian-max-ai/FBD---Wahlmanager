79536ad93fd9a447deb8e5711e97387494ae025c
Bot-Entwickler <bot-entwickler@test.de>
2026-09-06 16:27:52 +0200

    Add debug logs

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const { Client, GatewayIntentBits, Collection, Partials } = require('discord.js'
 const { BOT_TOKEN } = require('./src/utils/config');
 
 http.createServer((req, res) => res.end('Bot läuft.')).listen(process.env.PORT || 3000);
+console.log('HTTP Server gestartet');
 
 if (!BOT_TOKEN) throw new Error('BOT_TOKEN is required.');
+console.log('BOT_TOKEN vorhanden:', !!BOT_TOKEN);
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
